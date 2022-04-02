4ab96056130578fae253780a5efc10f6f0833be8
younes  amara <younesamara00@gmail.com>
2022-04-02 22:32:51 +0000

    Added burndown chart

--- frontend/src/app/projet/projet.module.ts
+++ frontend/src/app/projet/projet.module.ts
@@ -1,45 +1,46 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
 
-import { ProjetRoutingModule } from './projet-routing.module';
-import { ProjetComponent } from './projet.component';
-import { HeaderComponent } from '../header/header.component';
-import { SubHeaderComponent } from '../sub-header/sub-header.component';
-import { ProjetListComponent } from './projet-list/projet-list.component';
-import { ProjetCardComponent } from './projet-card/projet-card.component';
-import { ProjetDetailComponent } from './projet-detail/projet-detail.component';
-import { ProjetFormComponent } from './projet-form/projet-form.component';
-import { FormsModule } from '@angular/forms';
-import { BacklogComponent } from '../backlog/backlog.component';
-import { SprintComponent } from '../sprint/sprint.component';
-import { StoryModule } from '../story/story.module';
-import { TacheModule } from '../tache/tache.module';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { JwtService } from '../shared/services/jwt.service';
-
+import {ProjetRoutingModule} from "./projet-routing.module";
+import {ProjetComponent} from "./projet.component";
+import {HeaderComponent} from "../header/header.component";
+import {SubHeaderComponent} from "../sub-header/sub-header.component";
+import {ProjetListComponent} from "./projet-list/projet-list.component";
+import {ProjetCardComponent} from "./projet-card/projet-card.component";
+import {ProjetDetailComponent} from "./projet-detail/projet-detail.component";
+import {ProjetFormComponent} from "./projet-form/projet-form.component";
+import {FormsModule} from "@angular/forms";
+import {BacklogComponent} from "../backlog/backlog.component";
+import {SprintComponent} from "../sprint/sprint.component";
+import {StoryModule} from "../story/story.module";
+import {TacheModule} from "../tache/tache.module";
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {JwtService} from "../shared/services/jwt.service";
+import {BdchartComponent} from "./bdchart/bdchart.component";
+import {NgApexchartsModule} from "ng-apexcharts";
 
 @NgModule({
   declarations: [
     ProjetComponent,
-    HeaderComponent, 
+    HeaderComponent,
     SubHeaderComponent,
     ProjetListComponent,
     ProjetCardComponent,
     ProjetDetailComponent,
     ProjetFormComponent,
     BacklogComponent,
-    SprintComponent
+    SprintComponent,
+    BdchartComponent,
   ],
   imports: [
     CommonModule,
     FormsModule,
     ProjetRoutingModule,
     StoryModule,
-    TacheModule
+    TacheModule,
+    NgApexchartsModule,
   ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: JwtService, multi: true }
-  ],
-  bootstrap: [ProjetComponent]
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: JwtService, multi: true}],
+  bootstrap: [ProjetComponent],
 })
-export class ProjetModule { }
+export class ProjetModule {}
