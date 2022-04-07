467d96aad780186d3c5968bf5d95650c318ea11b
younes  amara <younesamara00@gmail.com>
2022-04-07 15:40:45 +0000

    Auto stash before merge of "main" and "origin/main"

--- frontend/src/app/shared/services/tache.service.ts
+++ frontend/src/app/shared/services/tache.service.ts
@@ -1,19 +1,18 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {TacheModel} from '../models/tache.model';
-import {environment} from '../../../environments/environment';
-import {Observable} from 'rxjs';
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {TacheModel} from "../models/tache.model";
+import {environment} from "../../../environments/environment";
+import {Observable} from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class TacheService {
   readonly baseUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   postTaches(tache: TacheModel): Observable<any> {
-    return this.http.post(this.baseUrl + '/api/Taches', tache);
+    return this.http.post(this.baseUrl + "/Taches", tache);
   }
 }
